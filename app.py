bc34d8d80a1436383c879e3826e9cd3f4da4b4a7
Abhishek <abhikr.ak.2001@gmail.com>
2024-10-03 23:07:38 +0530

    basics

---
+++ app.py
@@ -0,0 +1,24 @@
+# x = 10
+# x = x + 20
+# print(x)
+# print(type(x))
+
+# x = "abhishek"
+# print(x)
+# print(type(x))
+
+# name = input("what is your name?")
+# print('Welcom' , name)
+
+# x = 9
+
+# if x >= 10:
+#     print("grater than 10")
+# if x < 10:
+#     print("less than 10")
+
+# astr = 'Hello'
+# istr = 0
+
+# print(int(astr))
+
