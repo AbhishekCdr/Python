ab1a751dbf9d6fdb6cebb728f95a7878df396db8
Abhishek <abhikr.ak.2001@gmail.com>
2024-10-06 12:02:39 +0530

    strings

---
+++ 2. Data_Types/data_types.py
@@ -0,0 +1,111 @@
+
+# DATA TYPES:
+
+# Strings
+
+message = "Hello World"
+
+"""
+print(message)
+
+# length of the message
+print(len(message))
+
+# access location
+print("location at 0th index", message[4])
+
+# access range 
+print("hello from hello world: ", message[0:5])
+
+# 6th index till last
+print(message[6:])
+
+# lower case 
+print(message.lower())
+
+# upper case
+print(message.upper())
+
+# count method
+print(message.count('H'))
+ 
+# find method
+print(message.find('World'))
+# -> return -1 of not found
+
+# replace method
+new_message = message.replace('World', 'Universe')
+# not in-place method / saves it to new variable
+
+print(new_message) 
+
+# using placeholder for concatination
+greeting = 'Hello'
+name = 'Abhishek'
+
+greet = '{}, {}. Welcome!'.format(greeting,name)
+
+print(greet)
+
+# using f string / support on python 3.6+
+
+greetf = f'{greeting}, {name}. Welcome!'
+print(greetf)
+
+# find all attributes and methods available
+print(dir(name))
+
+# to get details insight 
+print(help(str))
+
+"""
+
+# Integer and Float
+
+"""
+
+Arithematic Operations:
+
+Addition: 3 + 2
+Subtraction: 3 - 2
+Multiplication: 3 * 2
+Division: 3 / 2
+Floor Division: 3 // 2
+Exponent:  3 ** 2
+Modulus: 3 % 2
+
+"""
+
+print(3/2)
+print(3//2) #-> removes decimal
+
+# incremanting values
+
+num = 1
+
+num += 1
+num *= 2
+
+print(num)
+
+# absolute value
+print(-10)
+
+# round value
+print(round(3.49, 1)) 
+# -> second parameter is to round value upto decimal places
+
+"""
+Comparision Operators > Returns boolean
+
+Equal: 3 == 2
+Not Equal: 3 != 2
+Greater Than: 3 > 2
+Less Than: 3 < 2
+Greater 
+"""
+
+
+
+
+
