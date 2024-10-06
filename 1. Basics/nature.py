cdd4f8c651dc57a9611d7ed2fb8b35274ece5436
Abhishek <abhikr.ak.2001@gmail.com>
2024-10-06 12:04:15 +0530

    interpreter

---
+++ 1. Basics/nature.py
@@ -0,0 +1,20 @@
+# Python is primarilay a single threaded language but it does support multithreading and multi processing 
+
+"""
+
+
+-> Single Threaded Nature
+
+Python has a global interpreter lock (GIL) which ecsure tat only one thread executes python bytecode at a time
+
+-> Multi Threaded Nature
+
+Python has the threadeing moudule using which we can create and manage the threads
+
+because of GIL multi-threading does't provide significant performance improvements for CPU bond task
+
+
+-> Multi Processing in Python
+in order to take full advantage  Pytonn offers multiprocessing module which avoids the GIL by spawning seperate processes each with its own python interpreter
+
+"""
---
+++ 1. Basics/python_interpreter.py
@@ -0,0 +1,17 @@
+"""
+
+PVM -> Python Virtual Machine
+
+https://www.youtube.com/watch?v=BkHdmAhapws
+
+https://medium.com/illumination/how-does-python-actually-work-9d00bf6dd02b#:~:text=When%20you%20run,%20python%20source.py.%20Here%20python%20is%20the%20interpretor,
+
+-> Get the compiled pyhton code
+
+python3 -m py_compile file.py
+
+-> the compiled code is saved in __pycache__ folder
+
+
+
+"""
