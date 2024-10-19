38772780813929579c06abaa272795f49f2fbc79
Abhishek <abhikr.ak.2001@gmail.com>
2024-10-19 21:42:26 +0530

    update

--- 1. Basics/python_interpreter.py
+++ 1. Basics/python_interpreter.py
@@ -12,6 +12,22 @@ python3 -m py_compile file.py
 
 -> the compiled code is saved in __pycache__ folder
 
+--> Python internally compiles the source code into and intermediate code called bytecode
+
+--------------
+
+Bytecode -> it is lower level representational of the python code
+
+we can look into __pycache__ folder and find the same with and extension of .pyc
+
+bytecode make the execution faster by skipping parsing and syntax analysis in subsequent runs
+
+---------------
+
+Once the bytecode is generated it is sent to PVM -> python virtual machine
+
+FLOW: Source Code -> ByteCode -> PVM
+
 
 
 """
